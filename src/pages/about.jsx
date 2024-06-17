ae7c41b355187980b3951b66c48d42958409f208
SopanhaSem <semsopanha072@gmail.com>
2024-06-17 14:43:59 +0700

    message

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,29 +3,29 @@ import React from "react";
 const AboutPage = () => {
   return (
     <>
-      <section class="font-poppins bg-white dark:bg-gray-900 relative">
+      <section className="font-poppins bg-white dark:bg-gray-900 relative">
         <div
           id="container"
-          class=" w-auto flex px-24 justify-center relative"
+          className=" w-auto flex px-24 justify-center relative"
         />
 
         <div
           id="container"
-          class="p-20 sm:p-16 md:p-20 lg:p-24 xl:p-20 w-auto flex flex-col md:flex-row px-4 sm:px-8 md:px-24 lg:px-24 xl:px-24 relative"
+          className="p-20 sm:p-16 md:p-20 lg:p-24 xl:p-20 w-auto flex flex-col md:flex-row px-4 sm:px-8 md:px-24 lg:px-24 xl:px-24 relative"
         >
-          <div class="mr-10">
+          <div className="mr-10">
             <img
-              class="rounded-lg min-w-[100px] w-full h-auto md:w-auto md:h-auto"
+              className="rounded-lg min-w-[100px] w-full h-auto md:w-auto md:h-auto"
               src="https://ucarecdn.com/833d0fe1-c3b5-4843-b62c-fed9467aceeb/imageOfMyself.jpeg"
               alt="image of myself"
             />
           </div>
-          <div class="w-full sm:w-[70%] md:w-[60%] lg:w-[50%]">
-            <h1 class="text-white font-bold text-3xl mt-6 mb-8">
+          <div className="w-full sm:w-[70%] md:w-[60%] lg:w-[50%]">
+            <h1 className="text-white font-bold text-3xl mt-6 mb-8">
               Hey it's me, Aydin Vesali Moghaddam
             </h1>
 
-            <p class="text-white w-full sm:w-[35rem] md:w-[30rem] lg:w-[25rem] mb-10">
+            <p className="text-white w-full sm:w-[35rem] md:w-[30rem] lg:w-[25rem] mb-10">
               I'm Aydin, a 18-year-old high schooler with a passion for web
               development. My tech journey started with HTML, CSS, and
               JavaScript, and I was hooked by the thrill of crafting dynamic,
@@ -37,16 +37,16 @@ const AboutPage = () => {
 
             <div
               id="social"
-              class="flex flex-wrap justify-start items-center gap-4"
+              className="flex flex-wrap justify-start items-center gap-4"
             >
               <a
                 rel="noopener"
                 target="_blank"
                 href="https://github.com/iam-aydin"
-                class="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
+                className="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
               >
                 <img
-                  class="mr-2 hover:scale-105 transition duration-300 ease-in-out"
+                  className="mr-2 hover:scale-105 transition duration-300 ease-in-out"
                   src="https://ucarecdn.com/1f465c47-4849-4935-91f4-29135d8607af/github2.svg"
                   width="20px"
                   height="20px"
@@ -58,10 +58,10 @@ const AboutPage = () => {
                 rel="noopener"
                 target="_blank"
                 href="https://www.linkedin.com/in/aydin-vesali-moghaddam-82a860275/"
-                class="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
+                className="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
               >
                 <img
-                  class="mr-2 hover:scale-105 transition duration-300 ease-in-out"
+                  className="mr-2 hover:scale-105 transition duration-300 ease-in-out"
                   src="https://ucarecdn.com/95eebb9c-85cf-4d12-942f-3c40d7044dc6/linkedin.svg"
                   width="20px"
                   height="20px"
@@ -73,10 +73,10 @@ const AboutPage = () => {
                 rel="noopener"
                 target="_blank"
                 href="https://twitter.com/ichbinaydin"
-                class="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
+                className="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
               >
                 <img
-                  class="mr-2 hover:scale-105 transition duration-300 ease-in-out"
+                  className="mr-2 hover:scale-105 transition duration-300 ease-in-out"
                   src="https://ucarecdn.com/82d7ca0a-c380-44c4-ba24-658723e2ab07/"
                   width="20px"
                   height="20px"
